9e28d718c88d9d04e2edf4e00e32d63f9d8602b8
Sri <117728774+Sridharshan2004@users.noreply.github.com>
2023-06-05 15:04:24 +0530

    Update http-commons.js

--- src/Service/http-commons.js
+++ src/Service/http-commons.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance=axios.create({
 
-     baseURL: 'http://ec2-15-207-249-115.ap-south-1.compute.amazonaws.com:8085/api',
+     baseURL: 'http://http://ec2-13-233-67-77.ap-south-1.compute.amazonaws.com//api',
    // baseURL: 'http://mindjobcard.ap-south-1.elasticbeanstalk.com/api',
     
 
